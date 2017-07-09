b29971a15deac9f164fcb1b7f5b7585d87a5566d
Joel Arvidsson <joel@oblador.se>
2017-07-09 22:16:21 +0200

    Fix loki/configure-react setup

--- src/targets/chrome/configure-storybook.js
+++ src/targets/chrome/configure-storybook.js
@@ -1,7 +1,11 @@
 /* global window */
 /* eslint-disable global-require, import/no-extraneous-dependencies, import/no-unresolved */
 
-if (typeof window === 'object') {
-  const { getStorybook } = require('@storybook/react');
-  window.loki = { getStorybook };
+function configureStorybook() {
+  if (typeof window === 'object') {
+    const { getStorybook } = require('@storybook/react');
+    window.loki = { getStorybook };
+  }
 }
+
+module.exports = configureStorybook;
