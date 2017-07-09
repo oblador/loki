65a5be44ecc69e0c88258e4eccaa436b44a61ff6
Joel Arvidsson <joel@oblador.se>
2017-07-09 14:36:13 +0200

    Add move cli executable to bin folder

--- src/cli.js
+++ src/cli.js
@@ -1,20 +1,23 @@
-#!/usr/bin/env node
-
-const args = process.argv.slice(2);
-const argv = require('minimist')(args);
+const minimist = require('minimist');
 const { die } = require('./console');
 const test = require('./commands/test');
 
-const command = argv._[0];
+function run() {
+  const args = process.argv.slice(2);
+  const argv = minimist(args);
+  const command = argv._[0];
 
-switch (command) {
-  case undefined:
-  case 'test': {
-    test(args);
-    break;
-  }
-  default: {
-    die(`Invalid command ${command}`);
-    break;
+  switch (command) {
+    case undefined:
+    case 'test': {
+      test(args);
+      break;
+    }
+    default: {
+      die(`Invalid command ${command}`);
+      break;
+    }
   }
 }
+
+module.exports = run;
