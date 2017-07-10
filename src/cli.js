3c5eeffdf869fc4ca4d7d35a391d997aeb8a102a
Joel Arvidsson <joel@oblador.se>
2017-07-10 03:00:08 +0200

    Output loki version in CLI

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,13 @@
 const minimist = require('minimist');
-const { die } = require('./console');
+const { die, bold } = require('./console');
 const test = require('./commands/test');
+const { version } = require('../package.json');
 
 function run() {
   const args = process.argv.slice(2);
   const argv = minimist(args);
   const command = argv._[0];
+  bold(`loki v${version}`);
 
   switch (command) {
     case undefined:
--- src/console.js
+++ src/console.js
@@ -5,9 +5,10 @@ const chalk = require('chalk');
 const info = message => console.log(chalk.dim(message));
 const warn = message => console.warn(chalk.yellow(message));
 const error = message => console.error(chalk.bold.red(message));
+const bold = message => console.log(chalk.bold(message));
 const die = message => {
   error(message);
   process.exit(1);
 };
 
-module.exports = { info, warn, error, die };
+module.exports = { info, warn, error, bold, die };
