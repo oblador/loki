e04b74c6201a02b09ee2cfc86fe3cbb2bad500bb
Joel Arvidsson <joel@oblador.se>
2017-07-12 16:25:12 +0200

    Revert react button story to English

--- examples/react/stories/index.js
+++ examples/react/stories/index.js
@@ -9,5 +9,5 @@ import { Button, Welcome } from '@storybook/react/demo';
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hej Button</Button>)
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
   .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
