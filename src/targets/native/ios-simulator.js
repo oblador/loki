1cde6271d8b7a3c38dfb6e75f0229b670921238a
Joel Arvidsson <joel@oblador.se>
2017-07-12 17:19:53 +0200

    Add retry mechanism for iOS screenshots

--- src/targets/native/ios-simulator.js
+++ src/targets/native/ios-simulator.js
@@ -1,7 +1,15 @@
-const createWebsocketTarget = require('./create-websocket-target');
+const fs = require('fs-extra');
 const osnap = require('osnap/src/ios');
+const { withRetries } = require('../../failure-handling');
+const createWebsocketTarget = require('./create-websocket-target');
 
-const saveScreenshotToFile = filename => osnap.saveToFile({ filename });
+const saveScreenshotToFile = withRetries(3)(async filename => {
+  await osnap.saveToFile({ filename });
+  const { size } = await fs.stat(filename);
+  if (size === 0) {
+    throw new Error('Screenshot failed ');
+  }
+});
 
 const createIOSSimulatorTarget = socketUri =>
   createWebsocketTarget(socketUri, 'ios', saveScreenshotToFile);
