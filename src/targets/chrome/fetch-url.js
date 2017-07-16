d7bdcfbd7a1d91353cd5d2d7a9e9f6d6e94da4cc
Joel Arvidsson <joel@oblador.se>
2017-07-16 16:27:56 +0800

    Reject promise instead of exiting when failing to fetch url

--- src/targets/chrome/fetch-url.js
+++ src/targets/chrome/fetch-url.js
@@ -2,16 +2,19 @@ const http = require('http');
 const url = require('url');
 
 function fetchUrl(urlToFetch) {
-  return new Promise(resolve => {
-    http.get(url.parse(urlToFetch), res => {
-      let body = '';
-      res.on('data', d => {
-        body += d;
-      });
-      res.on('end', () => {
-        resolve(body);
-      });
-    });
+  return new Promise((resolve, reject) => {
+    const options = Object.assign({ agent: false }, url.parse(urlToFetch));
+    http
+      .get(options, res => {
+        let body = '';
+        res.on('data', d => {
+          body += d;
+        });
+        res.on('end', () => {
+          resolve(body);
+        });
+      })
+      .on('error', reject);
   });
 }
 
