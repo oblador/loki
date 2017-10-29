add64c63211acb8a8f1e446eeb2dc51ea1040d12
Joel Arvidsson <joel@oblador.se>
2017-10-29 19:36:09 +0100

    Add Logo stories to react example

---
+++ examples/react/src/Logo.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const DELAY_URL_PREFIX = 'http://www.deelay.me';
+
+const Logo = ({ delay, logoUrl }) => (
+  <img
+    style={{ width: 75, height: 75 }}
+    alt=""
+    src={delay ? `${DELAY_URL_PREFIX}/${delay}/${logoUrl}` : logoUrl}
+  />
+);
+
+Logo.propTypes = {
+  delay: PropTypes.number,
+  logoUrl: PropTypes.string,
+};
+
+Logo.defaultProps = {
+  delay: 0,
+  logoUrl: 'https://loki.js.org/favicon.png',
+};
+
+export default Logo;
--- examples/react/stories/index.js
+++ examples/react/stories/index.js
@@ -5,9 +5,14 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
+import Logo from '../src/Logo';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
   .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+
+storiesOf('Logo', module)
+  .add('without delay', () => <Logo />)
+  .add('with 15s delay', () => <Logo delay={15000} />);
