793444508c8c0e0caecd5e6a00297466c772b7ca
Joel Arvidsson <joel@oblador.se>
2017-11-12 20:10:17 +0100

    Run sandboxed storybook bundle in sub process

--- src/targets/chrome/fetch-storybook.js
+++ src/targets/chrome/fetch-storybook.js
@@ -1,58 +1,51 @@
-const vm = require('vm');
-const debug = require('debug')('loki:chrome:fetchStories');
-const fetchUrl = require('./fetch-url');
-const getBrowserGlobals = require('./get-browser-globals');
+const cluster = require('cluster');
+const path = require('path');
 const { ServerError } = require('../../errors');
 
-async function createStorybookSandbox(baseUrl) {
-  debug(`Fetching iframe HTML and preview bundle JS from ${baseUrl}`);
-  const html = await fetchUrl(`${baseUrl}/iframe.html`);
-  const browser = getBrowserGlobals(html);
-  const scripts = browser.document.querySelectorAll('script[src]');
-  const previewSrc = Array.from(scripts)
-    .map(node => node.attributes.src.nodeValue)
-    .filter(src => src.match(/preview\.([a-f0-9]+\.)?bundle\.js/) !== -1)[0];
-
-  if (!previewSrc) {
-    throw new Error('Unable to locate preview bundle');
+function createError({ message, name, instructions }) {
+  switch (name) {
+    case 'ServerError':
+      return new ServerError(message, instructions);
+    default:
+      return new Error(message);
   }
-
-  const bundle = await fetchUrl(`${baseUrl}/${previewSrc}`);
-
-  debug('Creating js sandbox');
-  const sandbox = vm.createContext(browser);
-
-  debug('Executing storybook preview bundle');
-  vm.runInNewContext(bundle, sandbox);
-
-  return sandbox;
 }
 
-async function fetchStorybook(baseUrl = 'http://localhost:6006') {
-  let sandbox;
-  try {
-    sandbox = await createStorybookSandbox(baseUrl);
-  } catch (err) {
-    if (err.message && err.message.indexOf('ECONNREFUSED') !== -1) {
-      throw new ServerError(
-        'Failed fetching stories because the server is down',
-        `Try starting it with "yarn storybook" or pass the --port or --host arguments if it's not running at ${baseUrl}`
-      );
-    }
-    throw err;
-  }
+// Wrap this functionality in a killable sub process to be
+// able to avoid the main process waiting for storybook timers
+// running in the vm.
+function fetchStorybook(baseUrl = 'http://localhost:6006') {
+  return new Promise((resolve, reject) => {
+    cluster.setupMaster({
+      exec: path.join(__dirname, 'fetch-storybook-worker.js'),
+      silent: false,
+    });
 
-  const getStorybook = sandbox.window.loki && sandbox.window.loki.getStorybook;
-  if (!getStorybook) {
-    throw new Error(
-      "Loki addon not registered. Add `import 'loki/configure-react'` to your config.js file."
-    );
-  }
+    const worker = cluster.fork();
 
-  return getStorybook().map(component => ({
-    kind: component.kind,
-    stories: component.stories.map(story => story.name),
-  }));
+    worker.on('online', () => {
+      worker.send(baseUrl);
+    });
+
+    worker.on('message', message => {
+      const { error, storybook } = JSON.parse(message);
+      if (error) {
+        reject(createError(error));
+      } else {
+        resolve(storybook);
+      }
+      worker.kill();
+    });
+
+    worker.on('error', error => {
+      reject(error);
+      worker.kill();
+    });
+
+    worker.on('exit', code => {
+      reject(new Error(`Fetch storybook Worker exited with code ${code}`));
+    });
+  });
 }
 
 module.exports = fetchStorybook;
