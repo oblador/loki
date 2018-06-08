29d29a67abfb1f10dc848bdcb7898df8ff1fe4fc
Joel Arvidsson <joel@oblador.se>
2018-06-08 13:55:03 -0700

    Increase timeout for docker based tests

--- src/targets/chrome/create-chrome-target.spec.js
+++ src/targets/chrome/create-chrome-target.spec.js
@@ -1,5 +1,7 @@
 const createChromeDockerTarget = require('./docker');
 
+const DOCKER_TEST_TIMEOUT = 60000;
+
 const fetchStorybookFixture = async fixture => {
   const baseUrl = `file:${__dirname}/fixtures/storybook-${fixture}`;
   const target = createChromeDockerTarget({ baseUrl });
@@ -31,20 +33,32 @@ const storybook = [
 
 describe('createChromeTarget', () => {
   describe('.getStorybook', () => {
-    it('fetches stories from webpack dynamic bundles', async () => {
-      expect(await fetchStorybookFixture('dynamic')).toEqual(storybook);
-    });
+    it(
+      'fetches stories from webpack dynamic bundles',
+      async () => {
+        expect(await fetchStorybookFixture('dynamic')).toEqual(storybook);
+      },
+      DOCKER_TEST_TIMEOUT
+    );
 
-    it('fetches stories from static bundles', async () => {
-      expect(await fetchStorybookFixture('static')).toEqual(storybook);
-    });
+    it(
+      'fetches stories from static bundles',
+      async () => {
+        expect(await fetchStorybookFixture('static')).toEqual(storybook);
+      },
+      DOCKER_TEST_TIMEOUT
+    );
 
-    it('throws if not configured', async () => {
-      await expect(fetchStorybookFixture('unconfigured')).rejects.toEqual(
-        new Error(
-          "Loki addon not registered. Add `import 'loki/configure-react'` to your config.js file."
-        )
-      );
-    });
+    it(
+      'throws if not configured',
+      async () => {
+        await expect(fetchStorybookFixture('unconfigured')).rejects.toEqual(
+          new Error(
+            "Loki addon not registered. Add `import 'loki/configure-react'` to your config.js file."
+          )
+        );
+      },
+      DOCKER_TEST_TIMEOUT
+    );
   });
 });
