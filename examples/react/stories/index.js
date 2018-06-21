a455ef88929bba30fa196e40f8a76975bfeb9150
Joel Arvidsson <joel@oblador.se>
2018-06-21 23:56:08 +0200

    Improve pending request detection for chrome (#73)

--- examples/react/stories/index.js
+++ examples/react/stories/index.js
@@ -8,8 +8,13 @@ import { Button, Welcome } from '@storybook/react/demo';
 import Logo from '../src/Logo';
 import * as AnimatedComponent from '../src/AnimatedComponent';
 import DelayedComponent from '../src/DelayedComponent';
+import CursiveText from '../src/CursiveText';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+storiesOf('Text', module).add('with external font', () => (
+  <CursiveText>Hello CursiveText</CursiveText>
+));
 
 storiesOf('Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
@@ -19,9 +24,9 @@ storiesOf('Button', module)
 storiesOf('Asynchronous render', module)
   .add('Logo without delay', () => <Logo />)
   .add('Logo with 1s delay', () => <Logo delay={1000} />)
-  .add.async('add.async() with 1s delay', ({ done }) =>
+  .add.async('add.async() with 1s delay', ({ done }) => (
     <DelayedComponent delay={1000} onDone={done} />
-  );
+  ));
 
 storiesOf('Animation', module)
   .add('with CSS transition', () => <AnimatedComponent.CSSTransition />)
