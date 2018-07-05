55ab0be0c6b29c5b1fdc3bb9270de7bf78a9b938
Tomas Echeverri Valencia <tomechval@gmail.com>
2018-07-05 17:37:29 +0200

    Add visual test

---
+++ examples/react/src/StackedElements.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const StackedElements = () => [
+  <div>There should be another element below</div>,
+  <div>Hi, I'm the other element</div>,
+];
+export default StackedElements;
