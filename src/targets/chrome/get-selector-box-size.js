5a751557380018f8ec98c925689e34dff89993ab
Tomas Echeverri Valencia <tomechval@gmail.com>
2018-07-05 17:03:25 +0200

    Use Array.from to iterate over the NodeList

--- src/targets/chrome/get-selector-box-size.js
+++ src/targets/chrome/get-selector-box-size.js
@@ -4,26 +4,26 @@ const getSelectorBoxSize = (window, selector) => {
     throw new Error('Unable to find selector');
   }
 
-  return elements
-    .map(element => element.getBoundingClientRect())
-    .reduce((accumulator, { x, y, width, height }) => {
-      if (!accumulator) {
-        return { x, y, width, height };
-      }
+  return Array.from(elements, element =>
+    element.getBoundingClientRect()
+  ).reduce((accumulator, { x, y, width, height }) => {
+    if (!accumulator) {
+      return { x, y, width, height };
+    }
 
-      const xMin = Math.min(accumulator.x, x);
-      const yMin = Math.min(accumulator.y, y);
+    const xMin = Math.min(accumulator.x, x);
+    const yMin = Math.min(accumulator.y, y);
 
-      const xMax = Math.max(accumulator.x + accumulator.width, x + width);
-      const yMax = Math.max(accumulator.y + accumulator.height, y + height);
+    const xMax = Math.max(accumulator.x + accumulator.width, x + width);
+    const yMax = Math.max(accumulator.y + accumulator.height, y + height);
 
-      return {
-        x: xMin,
-        y: yMin,
-        width: xMax - xMin,
-        height: yMax - yMin,
-      };
-    });
+    return {
+      x: xMin,
+      y: yMin,
+      width: xMax - xMin,
+      height: yMax - yMin,
+    };
+  });
 };
 
 module.exports = getSelectorBoxSize;
