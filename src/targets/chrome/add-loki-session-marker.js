84265d78c86c997fb050acd08edeb2b4986ae8a1
Vasiliy Vanchuk <v.vanchuk@tut.by>
2018-10-27 17:38:48 +0400

    Add js marker

--- src/targets/chrome/add-loki-session-marker.js
+++ src/targets/chrome/add-loki-session-marker.js
@@ -1,4 +1,7 @@
-const addLokiSessionMarker = window =>
+const addLokiSessionMarker = window => {
   window.document.querySelector(':root').setAttribute('loki-test', true);
+  /* eslint-disable no-underscore-dangle, no-param-reassign */
+  window._isLokiTest = true;
+};
 
 module.exports = addLokiSessionMarker;
