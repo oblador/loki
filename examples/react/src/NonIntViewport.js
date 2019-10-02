0f84379185794b879684aa985c756d81988179fe
pawelk <pawel.knapik@gmail.com>
2019-10-02 13:11:45 +0200

    Add a test case for non-integer viewport
    Add reference images for Non-Integer Viewport

---
+++ examples/react/src/NonIntViewport.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const NonIntViewport = ({ children }) => (
+  <div style={{ 
+      width :'100%',
+      paddingTop : '17%',
+      backgroundColor : '#00f'
+  }}>
+        { children }
+  </div>
+);
+
+export default NonIntViewport;
