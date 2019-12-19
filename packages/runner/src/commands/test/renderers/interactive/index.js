2d8e0bb1038303344013a6d5e44f9361f21b4f39
Joel Arvidsson <joel@oblador.se>
2019-12-19 13:07:00 +0100

    Fix import of interactive renderer App

--- packages/runner/src/commands/test/renderers/interactive/index.js
+++ packages/runner/src/commands/test/renderers/interactive/index.js
@@ -2,7 +2,7 @@ const React = require('react');
 const importJsx = require('import-jsx');
 const { render } = require('ink');
 
-const App = importJsx('./app');
+const App = importJsx('./App');
 
 const renderInteractive = taskRunner => {
   const { unmount } = render(<App taskRunner={taskRunner} />);
