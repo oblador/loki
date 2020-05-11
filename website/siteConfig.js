337b277f7fc4da00e37affda5de08dd137c35c9d
Nik Donovic <32178288+ndonovic@users.noreply.github.com>
2020-05-11 11:01:07 -0700

    Update siteConfig.js
    Add Toptal to the list of users.

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -15,6 +15,12 @@ const users = [
     infoLink: 'https://www.bbc.com',
     pinned: true,
   },
+  {
+    caption: 'Toptal',
+    description: 'Toptal',
+    infoLink: 'https://www.toptal.com',
+    pinned: true,
+  },
 ];
 
 const siteConfig = {
