b5c2e118b67cabbe932ce9ac2759fa173a26a554
Joel Arvidsson <joel@oblador.se>
2020-07-24 10:42:01 +0200

    Support async CSF stories with createAsyncCallback

---
+++ packages/create-async-callback/src/index.web.js
@@ -0,0 +1,18 @@
+/* eslint-env browser */
+
+module.exports = function createAsyncCallback(win = window) {
+  const registerPendingPromise = win.loki && win.loki.registerPendingPromise;
+  let resolveAsyncStory;
+  if (registerPendingPromise) {
+    registerPendingPromise(
+      new Promise(function(resolve) {
+        resolveAsyncStory = resolve;
+      })
+    );
+  }
+  return () => {
+    if (resolveAsyncStory) {
+      resolveAsyncStory();
+    }
+  };
+};
