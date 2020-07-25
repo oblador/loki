31051b3633a3f82d23aaba813f1c14ccbc1f2ef4
Joel Arvidsson <joel@oblador.se>
2020-07-25 12:22:51 +0200

    Add google analytics to website

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -43,6 +43,7 @@ const siteConfig = {
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
   cname: 'loki.js.org',
+  gaTrackingId: 'UA-173568973-1',
 
   // Used for publishing and more
   projectName: 'loki',
