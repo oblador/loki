a6f6d019d82979fb767ef368e1173e2339f7d81b
Max Sosnov <crazymax101@gmail.com>
2020-10-06 23:33:31 +0700

    Fix graphicsMagick comparing images with different dimensions. Fixes #44

--- packages/diff-graphics-magick/src/create-graphics-magick-differ.js
+++ packages/diff-graphics-magick/src/create-graphics-magick-differ.js
@@ -1,9 +1,21 @@
 const fs = require('fs-extra');
 const gm = require('gm');
+const imageSize = require('image-size');
 
 function createGraphicsMagickDiffer(config) {
   return function getImageDiff(path1, path2, diffPath, tolerance) {
     const instanceConfig = { tolerance: tolerance / 100, ...config };
+
+    const path1Dimensions = imageSize(path1);
+    const path2Dimensions = imageSize(path2);
+
+    if (
+      path1Dimensions.height !== path2Dimensions.height ||
+      path1Dimensions.width !== path2Dimensions.width
+    ) {
+      return Promise.resolve(false);
+    }
+
     return new Promise((resolve, reject) => {
       gm.compare(
         path1,
