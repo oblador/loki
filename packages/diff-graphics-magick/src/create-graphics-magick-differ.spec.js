oblador/loki

commit 3f3d8641bf8baa1bac617db3d438168ad5941e8b

Fix graphicsMagick comparing images with different dimensions. Fixes #44 (#273)
* Fix graphicsMagick comparing images with different dimensions. Fixes #44

* Remove 3rd party lib

packages/diff-graphics-magick/src/create-graphics-magick-differ.js
@@ -1,30 +1,56 @@
 const fs = require('fs-extra');
 const gm = require('gm');
 
+const getSize = (filePath) =>
+  new Promise((resolve, reject) => {
+    gm(filePath).size((err, value) => {
+      if (err) {
+        return reject(err);
+      }
+
+      return resolve(value);
+    });
+  });
+
+const compare = (path1, path2, instanceConfig, diffPath) =>
+  new Promise((resolve, reject) => {
+    gm.compare(
+      path1,
+      path2,
+      { ...instanceConfig, file: diffPath },
+      (err, isEqual) => {
+        if (err) {
+          if (typeof err === 'string') {
+            reject(new Error(err));
+          } else {
+            reject(err);
+          }
+        } else {
+          if (isEqual) {
+            fs.remove(diffPath);
+          }
+          resolve(isEqual);
+        }
+      }
+    );
+  });
+
 function createGraphicsMagickDiffer(config) {
   return function getImageDiff(path1, path2, diffPath, tolerance) {
     const instanceConfig = { tolerance: tolerance / 100, ...config };
-    return new Promise((resolve, reject) => {
-      gm.compare(
-        path1,
-        path2,
-        { ...instanceConfig, file: diffPath },
-        (err, isEqual) => {
-          if (err) {
-            if (typeof err === 'string') {
-              reject(new Error(err));
-            } else {
-              reject(err);
-            }
-          } else {
-            if (isEqual) {
-              fs.remove(diffPath);
-            }
-            resolve(isEqual);
-          }
+
+    return Promise.all([getSize(path1), getSize(path2)]).then(
+      ([path1Dimensions, path2Dimensions]) => {
+        if (
+          path1Dimensions.height !== path2Dimensions.height ||
+          path1Dimensions.width !== path2Dimensions.width
+        ) {
+          return false;
         }
-      );
-    });
+
+        return compare(path1, path2, instanceConfig, diffPath);
+      }
+    );
   };
 }
 

commit d99b6e78edf147ab485a1a60fbe727b1428c1663

Fix tests and lint errors introduced by #273

packages/diff-graphics-magick/src/create-graphics-magick-differ.js
@@ -1,7 +1,7 @@
 const fs = require('fs-extra');
 const gm = require('gm');
 
-const getSize = (filePath) =>
+const getSize = filePath =>
   new Promise((resolve, reject) => {
     gm(filePath).size((err, value) => {
       if (err) {

packages/diff-graphics-magick/src/create-graphics-magick-differ.spec.js
@@ -1,9 +1,18 @@
+const mockSize = jest.fn(callback => callback(null, { with: 1, height: 1 }));
+
 const gm = require('gm');
 const createGraphicsMagickDiffer = require('./create-graphics-magick-differ');
 
-jest.mock('gm', () => ({
-  compare: jest.fn(),
-}));
+jest.mock('fs-extra');
+jest.mock('gm', () => {
+  const mockGm = jest.fn(() => ({ size: mockSize }));
+  mockGm.compare = jest.fn((path1, path2, config, callback) =>
+    callback(null, true)
+  );
+  return mockGm;
+});
+
+beforeEach(jest.clearAllMocks);
 
 describe('createGraphicsMagickDiffer', () => {
   const path1 = 'path1';
@@ -11,10 +20,24 @@ describe('createGraphicsMagickDiffer', () => {
   const diffPath = 'diffPath';
   const tolerance = 1;
 
-  it('should call gm.compare with the correct config', () => {
+  it('should return false for unequal sizes', async () => {
+    mockSize.mockImplementationOnce(callback =>
+      callback(null, { with: 100, height: 100 })
+    );
+    mockSize.mockImplementationOnce(callback =>
+      callback(null, { with: 100, height: 200 })
+    );
     const config = { asdf: '1234' };
     const differ = createGraphicsMagickDiffer(config);
-    differ(path1, path2, diffPath, tolerance);
+    const isEqual = await differ(path1, path2, diffPath, tolerance);
+    expect(isEqual).toBe(false);
+  });
+
+  it('should call gm.compare with the correct config', async () => {
+    const config = { asdf: '1234' };
+    const differ = createGraphicsMagickDiffer(config);
+    const isEqual = await differ(path1, path2, diffPath, tolerance);
+    expect(isEqual).toBe(true);
     expect(gm.compare).toHaveBeenCalledWith(
       path1,
       path2,
@@ -23,10 +46,10 @@ describe('createGraphicsMagickDiffer', () => {
     );
   });
 
-  it('should override tolerance with the config', () => {
+  it('should override tolerance with the config', async () => {
     const config = { tolerance: 0.2 };
     const differ = createGraphicsMagickDiffer(config);
-    differ(path1, path2, diffPath, tolerance);
+    await differ(path1, path2, diffPath, tolerance);
     expect(gm.compare).toHaveBeenCalledWith(
       path1,
       path2,