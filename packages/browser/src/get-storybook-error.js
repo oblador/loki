7bc302cd41767261f103f8af61115024ca8eec44
Joel Arvidsson <joel@oblador.se>
2020-11-24 19:40:23 +0100

    Surface render errors to user (#284)

---
+++ packages/browser/src/get-storybook-error.js
@@ -0,0 +1,11 @@
+const getStorybookError = window => {
+  const errorElement = window.document.querySelector(
+    '.sb-show-errordisplay #error-message'
+  );
+  if (errorElement) {
+    return errorElement.innerText;
+  }
+  return null;
+};
+
+module.exports = getStorybookError;
--- packages/browser/src/index.js
+++ packages/browser/src/index.js
@@ -5,6 +5,7 @@ const disableAnimations = require('./disable-animations');
 const disableInputCaret = require('./disable-input-caret');
 const disablePointerEvents = require('./disable-pointer-events');
 const getSelectorBoxSize = require('./get-selector-box-size');
+const getStorybookError = require('./get-storybook-error');
 const getStories = require('./get-stories');
 const populateLokiHelpers = require('./populate-loki-helpers');
 const setLokiIsRunning = require('./set-loki-is-running');
@@ -18,6 +19,7 @@ module.exports = {
   disableInputCaret,
   disablePointerEvents,
   getSelectorBoxSize,
+  getStorybookError,
   getStories,
   populateLokiHelpers,
   setLokiIsRunning,
