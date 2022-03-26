ede35c4d475575bfee8f1c8c42e8a2620f9c4658
Joel Arvidsson <joel@oblador.se>
2022-03-26 12:12:06 +0100

    Upgrade prettier to 2.6.1

--- packages/runner/src/commands/test/create-baseline-limited-batch-builder.js
+++ packages/runner/src/commands/test/create-baseline-limited-batch-builder.js
@@ -7,44 +7,43 @@ const FILL_RATE = 0.9;
 const DEFAULT_SIZE_LIMIT =
   FILL_RATE * (LAMBDA_PAYLOAD_LIMIT / BASE64_ENCODING_OVERHEAD);
 
-const createBaselineLimitedBatchBuilder = (
-  options,
-  baselineSizeLimit = DEFAULT_SIZE_LIMIT
-) => (tasks, batchSize) => {
-  let currentBatch = [];
-  let accumulatedBatchSize = 0;
-  let lastFileSize = 200 * 1024;
-  const batches = [currentBatch];
-  for (let i = 0; i < tasks.length; i++) {
-    const task = tasks[i];
-    const { referencePath } = getOutputPaths(
-      options,
-      task.task.configurationName,
-      task.task.kind,
-      task.task.story
-    );
-    let size;
-    try {
-      const stat = fs.statSync(referencePath);
-      // eslint-disable-next-line prefer-destructuring
-      size = stat.size;
-      lastFileSize = size;
-    } catch (e) {
-      size = lastFileSize;
+const createBaselineLimitedBatchBuilder =
+  (options, baselineSizeLimit = DEFAULT_SIZE_LIMIT) =>
+  (tasks, batchSize) => {
+    let currentBatch = [];
+    let accumulatedBatchSize = 0;
+    let lastFileSize = 200 * 1024;
+    const batches = [currentBatch];
+    for (let i = 0; i < tasks.length; i++) {
+      const task = tasks[i];
+      const { referencePath } = getOutputPaths(
+        options,
+        task.task.configurationName,
+        task.task.kind,
+        task.task.story
+      );
+      let size;
+      try {
+        const stat = fs.statSync(referencePath);
+        // eslint-disable-next-line prefer-destructuring
+        size = stat.size;
+        lastFileSize = size;
+      } catch (e) {
+        size = lastFileSize;
+      }
+      if (
+        currentBatch.length >= batchSize ||
+        (accumulatedBatchSize + size > baselineSizeLimit &&
+          currentBatch.length !== 0)
+      ) {
+        currentBatch = [];
+        accumulatedBatchSize = 0;
+        batches.push(currentBatch);
+      }
+      currentBatch.push(task);
+      accumulatedBatchSize += size;
     }
-    if (
-      currentBatch.length >= batchSize ||
-      (accumulatedBatchSize + size > baselineSizeLimit &&
-        currentBatch.length !== 0)
-    ) {
-      currentBatch = [];
-      accumulatedBatchSize = 0;
-      batches.push(currentBatch);
-    }
-    currentBatch.push(task);
-    accumulatedBatchSize += size;
-  }
-  return batches;
-};
+    return batches;
+  };
 
 module.exports = createBaselineLimitedBatchBuilder;
