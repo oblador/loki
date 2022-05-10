5a3796e1f84b7218528734a5a87ba0f12ef4249d
Rafal2228 <rafal2228@gmail.com>
2022-05-10 14:22:35 +0200

    Pass story parameters to tab options

--- examples/react/loki.config.js
+++ examples/react/loki.config.js
@@ -7,19 +7,9 @@ module.exports = {
       width: 1366,
       height: 768,
     },
-    'chrome.laptop.dark': {
-      target: 'chrome.docker',
-      width: 1366,
-      height: 768,
-      features: [{ name: 'prefers-color-scheme', value: 'dark' }],
-    },
     'chrome.iphone7': {
       target: 'chrome.docker',
       preset: 'iPhone 7',
-    },
-    'chrome.iphone7.dark': {
-      target: 'chrome.docker',
-      preset: 'iPhone 7 Dark Mode',
     },
     'chrome.a4': {
       target: 'chrome.docker',
--- examples/react/src/api/Media.stories.js
+++ examples/react/src/api/Media.stories.js
@@ -11,8 +11,22 @@ export const MediaAwareComponent = () => (
 
 MediaAwareComponent.storyName = 'with media queries';
 
-export const FeaturesAwareComponent = () => (
+export const LightSchemeComponent = () => (
   <div className="FeaturesAwareComponent" />
 );
 
-FeaturesAwareComponent.storyName = 'with color scheme queries';
+LightSchemeComponent.parameters = {
+  loki: { features: [{ name: 'prefers-color-scheme', value: 'light' }] },
+};
+
+LightSchemeComponent.storyName = 'with light scheme queries';
+
+export const DarkSchemeComponent = () => (
+  <div className="FeaturesAwareComponent" />
+);
+
+DarkSchemeComponent.parameters = {
+  loki: { features: [{ name: 'prefers-color-scheme', value: 'dark' }] },
+};
+
+DarkSchemeComponent.storyName = 'with dark scheme queries';
