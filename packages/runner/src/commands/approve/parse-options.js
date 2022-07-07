28a7661ac06b61438d4fb7ab254fe966311677e1
Mark Brockhoff <mark.brockhoff@mail.schwarz>
2022-07-07 11:50:37 +0200

    Allow overwriting the option set in config from the cli

--- packages/runner/src/commands/approve/parse-options.js
+++ packages/runner/src/commands/approve/parse-options.js
@@ -3,7 +3,7 @@ const minimist = require('minimist');
 const defaults = require('../test/default-options');
 
 function parseOptions(args, config) {
-  const argv = minimist(args);
+  const argv = minimist(args, { boolean: 'diffOnly' });
 
   const $ = (key) => argv[key] || config[key] || defaults[key];
 
@@ -11,7 +11,8 @@ function parseOptions(args, config) {
     outputDir: path.resolve($('output')),
     differenceDir: path.resolve($('difference')),
     referenceDir: path.resolve($('reference')),
-    diffOnly: $('diffOnly'),
+    // If --diffOnly was specified as a cli argument it should be used, otherwise config or default might overwrite it to true
+    diffOnly: args.includes('--diffOnly') ? argv.diffOnly : $('diffOnly'),
   };
 }
 
