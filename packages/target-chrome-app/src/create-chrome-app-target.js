c245102a9c93571a2e1cb41a9fc1edd1a42a4223
Gideon Pyzer <gjpyzer@gmail.com>
2023-08-04 11:21:01 +0100

    Use http server for file references in chrome.app

--- packages/target-chrome-app/src/create-chrome-app-target.js
+++ packages/target-chrome-app/src/create-chrome-app-target.js
@@ -1,6 +1,12 @@
 const debug = require('debug')('loki:chrome:app');
 const chromeLauncher = require('chrome-launcher');
 const CDP = require('chrome-remote-interface');
+const getRandomPort = require('find-free-port-sync');
+const {
+  getAbsoluteURL,
+  getLocalIPAddress,
+  createStaticServer,
+} = require('@loki/core');
 const { createChromeTarget } = require('@loki/target-chrome-core');
 
 function createChromeAppTarget({
@@ -8,8 +14,37 @@ function createChromeAppTarget({
   chromeFlags = ['--headless', '--disable-gpu', '--hide-scrollbars'],
 }) {
   let instance;
+  let staticServer;
+  let staticServerPath;
+  let staticServerPort;
+
+  let chromeUrl = getAbsoluteURL(baseUrl);
+  const isLocalFile = chromeUrl.indexOf('file:') === 0;
+
+  if (chromeUrl.indexOf('http://localhost') === 0 || isLocalFile) {
+    const ip = getLocalIPAddress();
+
+    if (!ip) {
+      throw new Error(
+        'Unable to detect local IP address, try passing --host argument'
+      );
+    }
+
+    if (isLocalFile) {
+      staticServerPort = getRandomPort();
+      staticServerPath = chromeUrl.substr('file:'.length);
+      chromeUrl = `http://${ip}:${staticServerPort}`;
+    } else {
+      chromeUrl = chromeUrl.replace('localhost', ip);
+    }
+  }
 
   async function start(options = {}) {
+    if (isLocalFile) {
+      staticServer = createStaticServer(staticServerPath);
+      staticServer.listen(staticServerPort);
+      debug(`Starting static file server at ${chromeUrl}`);
+    }
     const launchOptions = Object.assign(
       {
         chromeFlags,
@@ -30,6 +65,10 @@ function createChromeAppTarget({
     } else {
       debug('No chrome instance to kill');
     }
+
+    if (staticServer) {
+      staticServer.close();
+    }
   }
 
   async function createNewDebuggerInstance() {
@@ -47,7 +86,7 @@ function createChromeAppTarget({
     return client;
   }
 
-  return createChromeTarget(start, stop, createNewDebuggerInstance, baseUrl);
+  return createChromeTarget(start, stop, createNewDebuggerInstance, chromeUrl);
 }
 
 module.exports = createChromeAppTarget;
